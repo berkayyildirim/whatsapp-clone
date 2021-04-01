91e699f8eadb142fd58aca17cad356b0eaa1b48d
berkayyildirim <berkay.yildirim@yahoo.com>
2021-04-01 20:57:02 +0300

    implement axios for backend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
+import React, { useEffect, useState } from "react"
 import './App.css';
 import Sidebar from "./Sidebar"
 import Chat from "./Chat"
+import Pusher from "pusher-js"
+import axios from "./axios";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    axios.get("/messages/sync")
+    .then(response => {
+      setMessages(response.data);
+    })
+  }, [])
+
+  useEffect(() => {
+    const pusher = new Pusher('1f31db359a61481032ab', {
+      cluster: 'eu'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (newMessage) => {
+      setMessages([...messages, newMessage])
+    });
+
+    // cleanup function
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [messages]);
+
+  console.log(messages);
+
   return (
     <div className="app">
       <div className="app__body">
         <Sidebar />
-        <Chat />
+        <Chat messages={messages}/>
       </div>
     </div>
   );
