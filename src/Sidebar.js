1a1cab8a5c5f597a79b708a949c99d2f136f84e3
berkayyildirim <berkay.yildirim@yahoo.com>
2021-04-01 13:11:55 +0300

    build sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,14 +1,44 @@
 import React from 'react'
 import "./Sidebar.css"
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import { Avatar, IconButton } from '@material-ui/core';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { SearchOutlined } from '@material-ui/icons';
+import SidebarChat from "./SidebarChat";
+
+// npm i @material-ui/core
+// npm i @material-ui/icons
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <h1>I am a sidebar</h1>
             <div className="sidebar__header">
+                <Avatar src="https://avatars.githubusercontent.com/u/54432776?v=4" />
                 <div className="sidebar__headerRight">
-
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
                 </div>
+            </div>
+
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="Seacrh of start new chat" type="text" />
+                </div>
+            </div>
+
+            <div className="sidebar__chats">
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
             </div>
         </div>
     )
